d11b46a04c007ed09b4274f33a2edc6ba6cf7299
BebsYanski <bebongnchuy@gmail.com>
2025-08-01 11:31:39 +0100

    feat(navigation): add nav bar for switching between the diferent screens

---
+++ app/movie/[id].jsx
@@ -0,0 +1,16 @@
+import { useLocalSearchParams } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Text>Movie details: {id}</Text>
+    </View>
+  );
+};
+
+export default MovieDetails;
+
+const styles = StyleSheet.create({});
